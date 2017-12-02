01c9a65d6e32c0b765a42f1df268fc3c73b310a9
Ashton Reed <dreamergirl411@gmail.com>
2017-12-02 04:50:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  let matchingDrivers = drivers.filter(function(driver){
+    if (name.toLowerCase() === driver.toLowerCase()){
+      return true
+    } else {
+      return false
+    }
+  })
+  return matchingDrivers
+}
+
+function fuzzyMatch(drivers, letters) {
+  let matchingNames = drivers.filter(function(driver){
+    if (name === letters){
+      return true
+    } else {
+      return false
+    }
+    return matchingNames
+  })
+}
+
+function matchName(drivers, name) {
+  let matchingDrivers = drivers.filter(function(driver){
+    if (name === driver) {
+      return true
+    } else {
+      return false
+    }
+    return matchingDrivers
+  })
+}
+ 
